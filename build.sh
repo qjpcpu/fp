d0513673d296848b43910e871181b68a241b53d4
JasonQu <qjpcpu@gmail.com>
2022-06-29 16:35:39 +0800

    update badge

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 COVERAGE=`go test -coverprofile c.out |grep coverage |grep -oE '[0-9]+[^%]*'`
 echo "Coverage: ${COVERAGE}%"
-curl -s "https://img.shields.io/badge/coavrege-$COVERAGE-green" > codcov.svg
+curl -s "https://img.shields.io/badge/coverage-$COVERAGE-green" > codcov.svg
